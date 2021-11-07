508cb1d4b1262339d2c9c184497c53bfbe30b49d
jakkie <jakubkielbasiewicz@gmail.com>
2021-11-07 12:03:21 +0100

    header file

---
+++ containers/grayscale/compression.cpp
@@ -0,0 +1,14 @@
+#include "compression.h"
+
+std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(const std::array<std::array<uint8_t, width>, height>&){
+
+}
+
+
+std::array<std::array<uint8_t, width>, height> decompressGrayscale(const std::vector<std::pair<uint8_t, uint8_t>>&){
+
+}
+
+void printMap(const std::array<std::array<uint8_t, width>, height>&){
+    
+}
